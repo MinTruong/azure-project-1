8fa1e9690249b217c7c31f831738b42effdf23c9
MinhTruong <minhcm2@gmail.com>
2022-08-11 11:00:06 +0700

    finish pj

--- terraform/main.tf
+++ terraform/main.tf
@@ -178,7 +178,7 @@ resource "azurerm_availability_set" "app_set" {
 #                     Virtual Machine                          #
 ################################################################
 resource "azurerm_virtual_machine" "app_vm" {
-  count = var.count
+  count = var.counter
   name                  = "app_vm-${count.index}"
   location              = var.location
   resource_group_name   = var.resource_group
@@ -214,45 +214,10 @@ resource "azurerm_virtual_machine" "app_vm" {
   }
 
   depends_on = [
-    azurerm_network_interface.app_interface1,
+    azurerm_network_interface.app_interface,
     azurerm_availability_set.app_set
   ]
 }
 
-resource "azurerm_virtual_machine" "app_vm2" {
-  name                  = "app_vm2"
-  location              = var.location
-  resource_group_name   = var.resource_group
-  network_interface_ids = ["${azurerm_network_interface.app_interface2.id}"]
-  vm_size               = "Standard_DS1_v2"
-  availability_set_id = azurerm_availability_set.app_set.id
-  # This means the OS Disk will be deleted when Terraform destroys the Virtual Machine
-  # NOTE: This may not be optimal in all cases.
-  delete_os_disk_on_termination = true
 
-  storage_image_reference {
-    id = "${data.azurerm_image.custom.id}"
-  }
 
-  storage_os_disk {
-    name              = "osdisk-2"
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
-    managed_disk_type = "Standard_LRS"
-  }
-
-  os_profile {
-    computer_name  = "hostname2"
-    admin_username = var.username
-    admin_password = var.password
-  }
-
-  os_profile_linux_config {
-    disable_password_authentication = false
-  }
-
-  depends_on = [
-    azurerm_network_interface.app_interface2,
-    azurerm_availability_set.app_set
-  ]
-}
